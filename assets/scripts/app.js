b440a18f06e5f028d8e8b4ce129dba64e42882e8
Qargo1 <129512956+Qargo1@users.noreply.github.com>
2024-06-02 19:19:09 +0300

    clean_code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,96 +1,95 @@
 const submitBtn = document.querySelector('#new-project-btn');
 const activeProjectsList = document.querySelector('#active-projects-list');
 const finishedProjectsList = document.querySelector('#finished-projects-list');
-const allProjects = [];
+allProjects = [];
 
-class NewProject {
+class Project {
     static id = 0;
 
-    constructor(projectName, date, description, additionalInfo) {
-        this.id = NewProject.id++;
-        this._active = true;
+    constructor(name, date, description, additionalInfo) {
+        this.id = Project.id++;
+        this.isActive = true;
         this.element = null;
 
-        this.projectName = projectName;
+        this.name = name;
         this.date = date;
         this.description = description;
         this.additionalInfo = additionalInfo;
     }
 
-    createNewCard() {
-        this.element = this._createCardElement();
-        activeProjectsList.appendChild(this.element);
+    toggleStatus() {
+        this._active = !this._active;
+        this.render();
     }
 
-    _cardElement(tag, value, parent) {
-        const newElement = document.createElement(tag);
-        newElement.textContent = value;
-        parent.appendChild(newElement);
-        return newElement;
-    }
+    createCardElement() {
+        const card = document.createElement('li');
+        card.classList.add('card');
+        card.setAttribute('data-extra-info', this.additionalInfo);
+        card.setAttribute('id', this.id);
+        
+        const createCardContent = (tag, text) => {
+            const element = document.createElement(tag);
+            element.textContent = text;
+            card.appendChild(element);
+            return element;
+        }
 
-    _createCardElement() {
-        const newList = document.createElement('li');
-        newList.classList.add('card');
-        newList.setAttribute('data-extra-info', this.additionalInfo);
-        newList.setAttribute('id', this.id);
-
-        this._cardElement('h2', this.projectName, newList);
-        this._cardElement('p', this.description, newList);
-        this._cardElement('p', this.date, newList);
-
-        const projectBtnMoreInfoTag = this._cardElement('button', 'More Info', newList);
-        projectBtnMoreInfoTag.classList.add('alt');
-        projectBtnMoreInfoTag.addEventListener('click', () => {
+        createCardContent('h2', this.name);
+        createCardContent('p', this.description);
+        createCardContent('p', this.date);
+        
+        const moreInfoBtn = createCardContent('button', 'More Info');
+        moreInfoBtn.classList.add('alt');
+        moreInfoBtn.addEventListener('click', () => {
             alert(`More Info: ${this.additionalInfo}`);
         });
-
-        const projectBtnFinishTag = this._cardElement('button', 'Finish', newList);
-        projectBtnFinishTag.addEventListener('click', () => {
-            this.updateStatus()
+        
+        const statusBtn = createCardContent('button', 'Finish');
+        statusBtn.addEventListener('click', () => {
+            this.toggleStatus()
+            statusBtn.textContent = this._active ? 'Finish' : 'Activate';
         });
-
-        return newList;
+        
+        this.element = card;
+        this.render();
     }
 
-    updateStatus() {
+    render() {
         if (this.element) {
-            this._active = !this._active
             this.element.remove();
-            const projectBtnFinishTag = this.element.querySelector('button:not(.alt)');
-
-            projectBtnFinishTag.textContent = this._active ? 'Finish' : 'Activate';
-            (this._active ? activeProjectsList : finishedProjectsList).appendChild(this.element);
         }
+        (this._active ? finishedProjectsList : activeProjectsList).appendChild(this.element);
     }
 }
 
+class App {
+    static init() {
+        const name = document.querySelector('#new-project-name').value.trim();
+        const date = document.querySelector('#new-project-date').value.trim();
+        const description = document.querySelector('#new-project-description').value.trim();
+        const additionalInfo = document.querySelector('#new-project-additional-info').value.trim();
+
+        if (!name) {
+            alert('Please enter a project name');
+            return;
+        }
+
+        const project = new Project(name, date, description, additionalInfo);
+        allProjects.push(project);
+        project.createCardElement();
+        App.clearInputs();
+    };
+
+    static clearInputs() {
+        document.querySelector('#new-project-name').value = '';
+        document.querySelector('#new-project-date').value = '';
+        document.querySelector('#new-project-description').value = '';
+        document.querySelector('#new-project-additional-info').value = '';
+    }
+}
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault();  // Prevent the form from submitting
-
-    const projectName = document.querySelector('#new-project-name');
-
-    if (!projectName.value.trim()) {
-        alert('Please enter a project name');
-        return;
-    }
-
-    const date = document.querySelector('#new-project-date');
-    const description = document.querySelector('#new-project-description');
-    const additionalInfo = document.querySelector('#new-project-additional-info');
-
-    const newProject = new NewProject(
-        projectName.value.trim(), 
-        date.value.trim(), 
-        description.value.trim(), 
-        additionalInfo.value.trim()
-    );
-    allProjects.push(newProject);
-    newProject.createNewCard();
-
-    projectName.value = '';
-    date.value = '';
-    description.value = '';
-    additionalInfo.value = '';
+    App.init();
 });
